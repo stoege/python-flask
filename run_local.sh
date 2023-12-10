e9031fa60027f0f2d1cc66dd5b7663b09174d67e
Christian Henschel <christian@henschel.ch>
2023-12-10 10:55:32 +0100

    docker compose local renamed and exclude docker-compose.yaml

--- run_local.sh
+++ run_local.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-docker compose build
+DOCKER_COMPOSE_FILE="docker-compose-local.yaml"
 
-docker compose up
+docker compose -f $DOCKER_COMPOSE_FILE build
+
+docker compose -f $DOCKER_COMPOSE_FILE up
 
 exit 0
